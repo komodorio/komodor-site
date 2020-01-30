ebccedcb78889d5bddc50baca5c426a4a8bb3f8b
Itiel Shwartz <itielshw@gmail.com>
2020-01-30 20:55:36 +0200

    first commit

---
+++ js/function.js
@@ -0,0 +1,14 @@
+/* ----------------------------------------------------------------------------------------
+* Author        : Awaiken
+* Template Name : App Launch - App Landing Page HTML5 Template
+* File          : Custom JS
+* Version       : 1.0
+* ---------------------------------------------------------------------------------------- */
+(function ($) {
+    "use strict";
+	
+	var $window = $(window); 
+	
+	
+
+})(jQuery);
